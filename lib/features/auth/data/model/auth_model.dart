4b823a334fe8b10a7be1ce3e1884740c21b0ba7b
Ahmed Samir <ahmedsami.r@yahoo.com>
2024-04-06 16:53:19 +0200

    create auth model

---
+++ lib/features/auth/data/model/auth_model.dart
@@ -0,0 +1,25 @@
+class AuthModel {
+    String? name;
+  String? email;
+  String? userId;
+  String? adress;
+  int? phoneNumber;  
+  AuthModel({required this.email, required this.name, required this.userId,
+   required this.adress, required this.phoneNumber});
+  AuthModel.fromJson({required Map<String, dynamic> data}) {
+    name = data['name'];
+    email = data['email'];
+    userId = data['userId'];
+    adress = data['adress'];
+    phoneNumber = data['phoneNumber'];
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'email': email,
+      'userId': userId,
+      'phoneNumber': phoneNumber,
+      'adress': adress,
+    };
+  }
+}
