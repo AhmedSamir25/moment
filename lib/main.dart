26db60e6adfa82fe051728fcf4b073138e2e5432
Ahmed Samir <ahmedsami.r@yahoo.com>
2024-04-07 21:25:01 +0200

    link firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:moment/features/auth/presentation/view/sign_up.dart';
+import 'package:moment/firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,);
+  runApp(const MomentApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MomentApp extends StatelessWidget {
+  const MomentApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
