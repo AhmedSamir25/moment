7e10caf9c7ce841be12503ab8d8f2ae81d1fe949
Ahmed Samir <ahmedsami.r@yahoo.com>
2024-04-09 00:32:54 +0200

    sign in logic

--- lib/features/auth/presentation/view/widgets/pick_image_view_body.dart
+++ lib/features/auth/presentation/view/widgets/pick_image_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moment/core/theme/text_style.dart';
 import 'package:moment/features/auth/presentation/logic/auth_cubit/auth_cubit.dart';
+import 'package:moment/features/auth/presentation/logic/auth_cubit/auth_state.dart';
 import 'package:moment/features/auth/presentation/logic/user_image_cubit/user_image_cubit.dart';
 import 'package:moment/features/auth/presentation/view/widgets/custom_elevated_button.dart';
 
@@ -11,102 +12,121 @@ class PickImageViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     final userPickedImage = BlocProvider.of<UserImageCubit>(context);
     final signUpCubit = BlocProvider.of<AuthCubit>(context);
-    return BlocBuilder<UserImageCubit, UserImageState>(
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is UploadImageToFireStoreSuccess) {
+          // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomeView(),));
+        }
+      },
       builder: (context, state) {
-        if (userPickedImage.imageFile == null) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 180),
-            child: Center(
-              child: Column(
-                children: [
-                   const CircleAvatar(
-                    radius: 85,
-                    backgroundColor: Colors.blue,
-                    child: Icon(
-                      Icons.person,
-                      size: 110,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        userPickedImage.pickImage();
-                      },
-                      child: Row(
-                        children: [
-                          const Icon(
-                            Icons.image,
-                            color: Colors.blue,
-                            size: 35,
-                          ),
-                          const SizedBox(
-                            width: 10,
-                            height:55,
-                          ),
-                          Text(
-                            'Select Image',
-                            style: StyleText.textStyle18.copyWith(
-                              color: Colors.blue
-                            ),
-                          )
-                        ],
-                      )),
-                      const SizedBox(height: 20,),
-                      CustomElevatedButton(onPressed: (){},buttonText: 'Continue',)
-                ],
-              ),
-            ),
-          );
-        } else {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 180),
-            child: Center(
-              child: Column(
-                children: [
-                  CircleAvatar(
-                      backgroundImage: FileImage(userPickedImage.imageFile!),
-                      radius: 85,
+        return BlocBuilder<UserImageCubit, UserImageState>(
+          builder: (context, state) {
+            if (userPickedImage.imageFile == null) {
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 40, vertical: 180),
+                child: Center(
+                  child: Column(
+                    children: [
+                      const CircleAvatar(
+                        radius: 85,
+                        backgroundColor: Colors.blue,
+                        child: Icon(
+                          Icons.person,
+                          size: 110,
+                        ),
                       ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        userPickedImage.pickImage();
-                      },
-                      child: Row(
-                        children: [
-                          const Icon(
-                            Icons.image,
-                            color: Colors.blue,
-                            size: 35,
-                          ),
-                          const SizedBox(
-                            width: 10,
-                            height:55,
-                          ),
-                          Text(
-                            'Change Image',
-                            style: StyleText.textStyle18.copyWith(
-                              color: Colors.blue,
-                          ),),
-                        ],
-                      )),
                       const SizedBox(
                         height: 20,
                       ),
-                  CustomElevatedButton(onPressed: ()async{
-                    await userPickedImage.uploadImage();
-                    await signUpCubit.uploadImageToFireStore(userImage: userPickedImage.imageLink!);
-                  },buttonText: 'Continue',)
-
-                ],
-              ),
-            ),
-          );
-        }
+                      ElevatedButton(
+                          onPressed: () {
+                            userPickedImage.pickImage();
+                          },
+                          child: Row(
+                            children: [
+                              const Icon(
+                                Icons.image,
+                                color: Colors.blue,
+                                size: 35,
+                              ),
+                              const SizedBox(
+                                width: 10,
+                                height: 55,
+                              ),
+                              Text(
+                                'Select Image',
+                                style: StyleText.textStyle18
+                                    .copyWith(color: Colors.blue),
+                              )
+                            ],
+                          )),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      CustomElevatedButton(
+                        onPressed: () {},
+                        buttonText: 'Continue',
+                      )
+                    ],
+                  ),
+                ),
+              );
+            } else {
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 40, vertical: 180),
+                child: Center(
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: FileImage(userPickedImage.imageFile!),
+                        radius: 85,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            userPickedImage.pickImage();
+                          },
+                          child: Row(
+                            children: [
+                              const Icon(
+                                Icons.image,
+                                color: Colors.blue,
+                                size: 35,
+                              ),
+                              const SizedBox(
+                                width: 10,
+                                height: 55,
+                              ),
+                              Text(
+                                'Change Image',
+                                style: StyleText.textStyle18.copyWith(
+                                  color: Colors.blue,
+                                ),
+                              ),
+                            ],
+                          )),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      CustomElevatedButton(
+                        onPressed: () async {
+                          await userPickedImage.uploadImage();
+                          await signUpCubit.uploadImageToFireStore(
+                              userImage: userPickedImage.imageLink!);
+                        },
+                        buttonText: 'Continue',
+                      )
+                    ],
+                  ),
+                ),
+              );
+            }
+          },
+        );
       },
     );
   }
--- lib/features/auth/presentation/view/widgets/sign_in_view_body.dart
+++ lib/features/auth/presentation/view/widgets/sign_in_view_body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moment/core/theme/text_style.dart';
+import 'package:moment/features/auth/presentation/logic/auth_cubit/auth_cubit.dart';
+import 'package:moment/features/auth/presentation/logic/auth_cubit/auth_state.dart';
 import 'package:moment/features/auth/presentation/view/widgets/auth_text_button.dart';
 import 'package:moment/features/auth/presentation/view/widgets/custom_elevated_button.dart';
 import 'package:moment/features/auth/presentation/view/widgets/title_body_auth.dart';
@@ -10,50 +13,73 @@ class SignInViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-    return  Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24.0),
-      child: ListView(
-        children: [
-          const TitleBodyAuth(titleText: 'Moment',
-          bodyText: 'Share your happy moment',),
-           UserSignInForm(
-            emailController: emailController,
-            passwordController: passwordController,
-          ),
-          CustomElevatedButton(onPressed: () {
-            if (emailController.text.isEmpty && passwordController.text.isEmpty ) {
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                          backgroundColor: Colors.red,
-                          content:
-                              Text('Check Password And Email Field Is Empty')));
-                  }
-                  else if (emailController.text.isEmpty) {
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                          backgroundColor: Colors.red,
-                          content: Text('Check Email Field Is Empty')));
-                    } else if (passwordController.text.isEmpty) {
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                          backgroundColor: Colors.red,
-                          content: Text('Check Password Field Is Empty')));
-                    }else{
-                      
-                    }
-          },
-          buttonText: 'Sign In',
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
+        final signInCubit = BlocProvider.of<AuthCubit>(context);
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+         if (state is UserSignSuccessState) {
+        //  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const PickImageView(),));
+       }else if(state is FeiledCreatedUserState){
+             ScaffoldMessenger.of(context).showSnackBar( SnackBar(
+              backgroundColor: Colors.red,content:
+               Text(state.massage)));
+       }
+      },
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: ListView(
             children: [
-              Text("Don’t Have Account?",style: StyleText.textStyle14,),
-              AuthSignTextButton(onPressed: () {
-                Navigator.pop(context);
-              },)
+              const TitleBodyAuth(
+                titleText: 'Moment',
+                bodyText: 'Share your happy moment',
+              ),
+              UserSignInForm(
+                emailController: emailController,
+                passwordController: passwordController,
+              ),
+              CustomElevatedButton(
+                onPressed: () {
+                  if (emailController.text.isEmpty &&
+                      passwordController.text.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        backgroundColor: Colors.red,
+                        content:
+                            Text('Check Password And Email Field Is Empty')));
+                  } else if (emailController.text.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        backgroundColor: Colors.red,
+                        content: Text('Check Email Field Is Empty')));
+                  } else if (passwordController.text.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        backgroundColor: Colors.red,
+                        content: Text('Check Password Field Is Empty')));
+                  } else {
+                    signInCubit.signIn(email: emailController.text,
+                     password: passwordController.text);
+                  }
+                },
+                buttonText: 'Sign In',
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don’t Have Account?",
+                    style: StyleText.textStyle14,
+                  ),
+                  AuthSignTextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  )
+                ],
+              )
             ],
-          )
-        ],
-      ),
+          ),
+        );
+      },
     );
   }
 }
